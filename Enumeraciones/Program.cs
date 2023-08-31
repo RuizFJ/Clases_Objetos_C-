d36b2cbbfaefa9f4e3076ca11dbcef579e792519
Francisco Ruiz <ruiz31348@gmail.com>
2023-08-31 12:02:30 -0600

    Se hicieron enumeraciones

---
+++ Enumeraciones/Program.cs
@@ -0,0 +1,21 @@
+﻿int estatusOprecion = 5;
+// Practicando enumeraciones para mejorar el entendimeinto de un codigo
+
+if (estatusOprecion ==(int) EstatusOperacion.Exitoso)
+{
+    //7
+} else if (estatusOprecion == (int) EstatusOperacion.ClienteNoEncontrado)
+{
+
+} else if (estatusOprecion == (int ) EstatusOperacion.ErrordeSistema)
+{
+
+}
+
+//Enumeraciones
+public enum EstatusOperacion
+{
+    Exitoso = 1,
+    ClienteNoEncontrado = 2,
+    ErrordeSistema = 5,
+}
